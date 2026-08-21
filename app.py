6bbeb02b03c0d1e6313edfee3d506e0fcf2838ee
ShivtejPatil2807 <shivtejpati2807@gmail.com>
2026-08-21 08:38:59 +0530

    Fix Quiz screen

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ def load_states():
         "score": 0 ,
         "current_questions": 0 ,
         "answered": False ,
-        "selected_answer": None
+        "selected_answer": None ,
+        "quiz_questions": []
     }
     for key , value in defaults.items():
 
@@ -65,5 +66,59 @@ if not st.session_state.start_quiz:
                 st.session_state.answered = False
 
                 st.rerun()
+else:
+    quiz_questions = st.session_state.quiz_questions
+    current = st.session_state.current_questions
 
+    if current >= len(quiz_questions):
+        st.success("🎉 Quiz Completed.")
 
+        st.metric("Your Score",f"{st.session_state.score} / {len(st.session_state.quiz_questions)}")
+        percentage = (st.session_state.score / len(quiz_questions)) * 100
+
+        st.write(f"### Percentage : {percentage :.1f}%")
+
+        if percentage >= 80:
+            st.balloons()
+            st.success("Excellent Work !")
+
+        elif percentage >= 50:
+            st.info("Good job ! Keep practicing.")
+
+        else:
+            st.warning("keep learning ! Try again.")
+
+        if st.button("Restart Quiz"):
+            st.session_state.quiz_start = False
+            st.session_state.score = 0
+            st.session_state.current_questions = 0
+            st.session_state.answered = False 
+            st.rerun()
+
+    else:
+        question = questions[current]
+
+        st.write(f"### Question ,{current +1 } / {len(quiz_questions)}")
+        st.progress(
+            (current + 1) / len(quiz_questions)
+        )
+
+        st.write("##")
+
+        st.subheader(question["question"])
+        selected = st.radio(
+            "Choose your answer",
+            question["options"],
+            key = f"question_{current}"
+        )
+
+        if not st.session_state.answered:
+            if st.button("Submit Answer"):
+
+                st.session_state.selected_answer = selected
+                st.session_state.answered = True
+
+                if selected == question["answer"]:
+                    st.session_state.score += 1
+
+                st.rerun()
